a48c36c34d3e43a59f6b308bbb98782d867af25d
DhavalDevangada <dhaval.devangada@pmcretail.com>
2024-11-03 01:15:45 +0530

    [Updated] Pre-conditions/Initialize rest client

--- src/test/java/com/qa/api/base/BaseTest.java
+++ src/test/java/com/qa/api/base/BaseTest.java
@@ -1,7 +1,17 @@
 package com.qa.api.base;
 
+import com.qa.api.client.RestClient;
+import org.testng.annotations.BeforeMethod;
+
 /**
  * We will nor write any @Test annotations here. Here we will write preconditions/ pre-requeties
  */
 public class BaseTest {
+
+    protected RestClient restClient; // Will be used only in the class which extend this class
+
+    @BeforeMethod
+    public void setUp(){
+        restClient = new RestClient();
+    }
 }
