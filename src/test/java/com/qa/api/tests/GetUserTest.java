1d36014718e4da9f106b8e37ca5ae1db927cc25b
DhavalDevangada <dhaval.devangada@pmcretail.com>
2024-11-03 01:18:36 +0530

    [Added] Code for POST call tests

--- src/test/java/com/qa/api/tests/GetUserTest.java
+++ src/test/java/com/qa/api/tests/GetUserTest.java
@@ -1,4 +1,33 @@
 package com.qa.api.tests;
 
-public class GetUserTest {
+import com.qa.api.base.BaseTest;
+import com.qa.api.constants.AuthType;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetUserTest extends BaseTest {
+
+    @Test
+    public void getUsersTest(){
+
+        Map<String,String> queryParams= new HashMap<>();
+        queryParams.put("name","Dharani");
+        queryParams.put("status","active");
+
+        Response response = restClient.get("public/v2/users", queryParams, null, AuthType.BEARER_TOKEN, ContentType.JSON);
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+
+    @Test
+    public void getSingleUserTest(){
+        Response response = restClient.get("public/v2/users/7501222", null, null, AuthType.BEARER_TOKEN, ContentType.JSON);
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+
+
 }
