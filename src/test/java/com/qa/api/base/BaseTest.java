7bee900d831e74825716593c48c449311c994ba0
DhavalDevangada <dhaval.devangada@pmcretail.com>
2024-11-03 23:28:37 +0530

    [Added] Parameters and baseUrl for another API

--- src/test/java/com/qa/api/base/BaseTest.java
+++ src/test/java/com/qa/api/base/BaseTest.java
@@ -1,7 +1,10 @@
 package com.qa.api.base;
 
 import com.qa.api.client.RestClient;
-import org.testng.annotations.BeforeMethod;
+import com.qa.api.manager.ConfigManager;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 /**
  * We will nor write any @Test annotations here. Here we will write preconditions/ pre-requeties
@@ -10,8 +13,12 @@ public class BaseTest {
 
     protected RestClient restClient; // Will be used only in the class which extend this class
 
-    @BeforeMethod
-    public void setUp(){
+    @Parameters({"baseUrl"})   //(These parameters helps us to read those parameters which we are reading from testng.xml file and if we are writing the parameter we need to supply the parameters)
+    @BeforeTest
+    public void setUp(@Optional String baseUrl){ //Optional annotation is coming from testng
+        if(baseUrl!=null){
+            ConfigManager.set("baseUrl",baseUrl); // It will not write it fo you in the config.properties file but it will update in the memory
+        }
         restClient = new RestClient();
     }
 }
