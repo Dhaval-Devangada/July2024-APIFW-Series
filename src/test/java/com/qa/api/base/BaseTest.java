acb62417bfee065a6ae2495704c7ccccc7bd8c4e
DhavalDevangada <dhaval.devangada@pmcretail.com>
2024-11-14 15:20:59 +0530

    [Added] BaseUrl for each project [Commented] Parameter-BaseUrl - As we are passing it as a parameter

--- src/test/java/com/qa/api/base/BaseTest.java
+++ src/test/java/com/qa/api/base/BaseTest.java
@@ -1,24 +1,30 @@
 package com.qa.api.base;
 
 import com.qa.api.client.RestClient;
-import com.qa.api.manager.ConfigManager;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 /**
  * We will nor write any @Test annotations here. Here we will write preconditions/ pre-requeties
  */
 public class BaseTest {
 
+    // We can use "BaseTest" class to maintain our constant as well and we can create constants in AppConstant as well
+    protected final static String BASE_URL_REQ_RES = "https://reqres.in";
+    protected final static String BASE_URL_PRODUCT = "https://fakestoreapi.com";
+    protected final static String BASE_URL_GOREST = "https://gorest.co.in";
+    protected final static String BASE_URL_CONTACTS = "https://thinking-tester-contact-list.herokuapp.com";
+    protected final static String BASE_URL_CIRCUIT = "https://ergast.com";
+    protected final static String BASE_URL_BASIC_AUTH = "https://the-internet.herokuapp.com";
+
+
     protected RestClient restClient; // Will be used only in the class which extend this class
 
-    @Parameters({"baseUrl"})   //(These parameters helps us to read those parameters which we are reading from testng.xml file and if we are writing the parameter we need to supply the parameters)
+//    @Parameters({"baseUrl"})   //(These parameters helps us to read those parameters which we are reading from testng.xml file and if we are writing the parameter we need to supply the parameters)
     @BeforeTest
-    public void setUp(@Optional String baseUrl){ //Optional annotation is coming from testng
-        if(baseUrl!=null){
-            ConfigManager.set("baseUrl",baseUrl); // It will not write it fo you in the config.properties file but it will update in the memory
-        }
+    public void setUp(){ //@Optional String baseUrl - Optional annotation is coming from testng
+//        if(baseUrl!=null){
+//            ConfigManager.set("baseUrl",baseUrl); // It will not write it fo you in the config.properties file but it will update in the memory
+//        }
         restClient = new RestClient();
     }
 }
