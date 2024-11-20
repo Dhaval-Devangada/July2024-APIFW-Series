6e9e0a7073c4e1e0c882fc15f57258677522cfd1
DhavalDevangada <dhaval.devangada@pmcretail.com>
2024-11-20 18:58:41 +0530

    [Added] Code to start and stop the wiremock server

---
+++ src/main/java/com/qa/api/mocking/WireMockSetup.java
@@ -0,0 +1,25 @@
+package com.qa.api.mocking;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+/**
+ * Here we will start and stop the Wiremock dummy server
+ * Our dummy API will be hosted here
+ */
+public class WireMockSetup {
+
+    public static WireMockServer server;
+    public static void createWireMockServer(){
+        server = new WireMockServer(8089);
+        WireMock.configureFor("localhost",8089); //host means if we are running this on any other machine than we need to give IP address of that machine
+        server.start(); //dummy server will start
+    }
+
+    public static void stopMockServer(){
+        server.stop();
+    }
+}
+
+//Mocks and behaviour both are same thing.
+// What is behaviour of the API/What exactly it will respond/request.
